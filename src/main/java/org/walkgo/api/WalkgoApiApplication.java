d847d75d31a773ed3b80065993c6f5fda7577f13
OtalTheMad <oscar454aqw@outlook.es>
2025-11-04 10:26:46 -0600

    Commit inicial

---
+++ src/main/java/org/walkgo/api/WalkgoApiApplication.java
@@ -0,0 +1,13 @@
+package org.walkgo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkgoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkgoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/walkgo/api/WalkgoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.walkgo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkgoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
