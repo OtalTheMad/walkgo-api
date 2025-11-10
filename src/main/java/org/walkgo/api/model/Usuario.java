d58cbe4a0f65541061cdbe1e07f60f9417008d02
OtalTheMad <oscar454aqw@outlook.es>
2025-11-10 09:56:37 -0600

    fix: Mapeos de tabla de Usuarios

--- src/main/java/org/walkgo/api/model/Usuario.java
+++ src/main/java/org/walkgo/api/model/Usuario.java
@@ -9,17 +9,37 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long _id;
 
+    @Column(name = "usuario", nullable = false, length = 30)
     private String _usuario;
+
+    @Column(name = "clave", nullable = false, length = 30)
     private String _clave;
+
+    @Column(name = "total_pasos", nullable = false)
     private Integer _total_pasos = 0;
+
+    @Column(name = "total_distancia_km", nullable = false, columnDefinition = "DECIMAL(65,0) DEFAULT 0")
     private Double _total_distancia_km = 0.0;
+
+    @Column(name = "total_pasos_semanales", nullable = false)
     private Integer _total_pasos_semanales = 0;
-    private Integer _rango_semanal;
+
+    @Column(name = "rango_semanal", nullable = false)
+    private Integer _rango_semanal = 0;
+
+    @Column(name = "creado_en", nullable = false)
     private LocalDateTime _creado_en = LocalDateTime.now();
+
+    @Column(name = "actualizado_en", nullable = false)
     private LocalDateTime _actualizado_en = LocalDateTime.now();
+
+    @Column(name = "estado", nullable = false)
     private Boolean _estado = true;
+
+    // -------- Getters and Setters --------
 
     public Long GetId() {
         return _id;
