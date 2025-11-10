86352eba0c4c15389700f011e14ef8dd57f867f1
OtalTheMad <oscar454aqw@outlook.es>
2025-11-10 09:06:50 -0600

    fix: Cambio de nombre a interfaz

--- src/main/java/org/walkgo/api/repository/UsuarioRepository.java
+++ src/main/java/org/walkgo/api/repository/UsuarioRepository.java
@@ -3,5 +3,5 @@ package org.walkgo.api.repository;
 import org.walkgo.api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
