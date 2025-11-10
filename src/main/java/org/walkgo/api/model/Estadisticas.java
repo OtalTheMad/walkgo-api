10f325fb0059df29e0d0fdbb3fdb9e7445fcb28c
OtalTheMad <oscar454aqw@outlook.es>
2025-11-10 09:15:20 -0600

    feat: Model Estadisticas completo

---
+++ src/main/java/org/walkgo/api/model/Estadisticas.java
@@ -0,0 +1,46 @@
+package org.walkgo.api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "estadisticas")
+public class Estadisticas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_estadistica")
+    private int id_estadistica;
+
+    @Column(name = "id_usuario")
+    private int id_usuario;
+
+    @Column(name = "km_recorrido")
+    private int km_recorrido;
+
+    @Column(name = "calorias_quemadas")
+    private String calorias_quemadas;
+
+    @Column(name = "clasificacion")
+    private String clasificacion;
+
+    @Column(name = "estado")
+    private String estado;
+
+    public int getId_estadistica() { return id_estadistica; }
+    public void setId_estadistica(int id_estadistica) { this.id_estadistica = id_estadistica; }
+
+    public int getId_usuario() { return id_usuario; }
+    public void setId_usuario(int id_usuario) { this.id_usuario = id_usuario; }
+
+    public int getKm_recorrido() { return km_recorrido; }
+    public void setKm_recorrido(int km_recorrido) { this.km_recorrido = km_recorrido; }
+
+    public String getCalorias_quemadas() { return calorias_quemadas; }
+    public void setCalorias_quemadas(String calorias_quemadas) { this.calorias_quemadas = calorias_quemadas; }
+
+    public String getClasificacion() { return clasificacion; }
+    public void setClasificacion(String clasificacion) { this.clasificacion = clasificacion; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+}
